cfa81be3fb349a4850a2ede1a6784da00269c916
AlexanderBarsc <82763122+AlexanderBarsc@users.noreply.github.com>
2023-05-21 11:47:30 +0200

    Update main.c

--- Kapitola 1/Cv1/main.c
+++ Kapitola 1/Cv1/main.c
@@ -1,4 +1,5 @@
 //Cviceni 1.1 Spoustejte na svem systemu program "Ahoj lidi!" Experimentuje s vynechavanim casti programu, abyste videli, jaka chybova hlaseni dostanete.
+//Civceni 1.2 Experimentovani zjistete, co se stane, jestlize arguement funkce printf tvoreny retezcem obsahuje \z, kde z je libovolny znak, o nemz jsme se nezminili
 
 #include "stdio.h"
 
