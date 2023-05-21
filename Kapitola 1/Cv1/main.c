e39c55903bf59f8a6861c7dfe103004693582ff5
AlexanderBarsc <82763122+AlexanderBarsc@users.noreply.github.com>
2023-05-21 11:46:59 +0200

    Hotove cviceni 1.1 a 1.2

---
+++ Kapitola 1/Cv1/main.c
@@ -0,0 +1,8 @@
+//Cviceni 1.1 Spoustejte na svem systemu program "Ahoj lidi!" Experimentuje s vynechavanim casti programu, abyste videli, jaka chybova hlaseni dostanete.
+
+#include "stdio.h"
+
+int main()
+{
+  printf("Ahoj lidi!");
+}
